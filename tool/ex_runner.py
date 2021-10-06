c523bede9604bbf35ab9adf3a803ad015a649364
Khashayar Etemadi <khaes@kth.se>
2021-10-06 23:25:30 +0200

    new versoin of tool and script added

--- tool/ex_runner.py
+++ tool/ex_runner.py
@@ -3,7 +3,9 @@ import os
 import multiprocessing as mp
 
 tools = {"Arja", "Cardumen", "Elixir", "JGenProg", "JKali", "JMutRepair", "Nopol", "NPEfix"}
-#tools = {"Arja"}
+
+
+# tools = {"Arja"}
 
 def process(repoName, repoUrl, branch, tool):
     exec_id = f"{repoName}_{tool}"
@@ -13,11 +15,13 @@ def process(repoName, repoUrl, branch, tool):
     os.system(f"mkdir {exec_id}_filtered_out")
 
     os.system(f"git clone {repoUrl} {exec_id}")
-    os.system(f"java -Xmx1g -classpath ./coming.jar fr.inria.coming.main.ComingMain -location ./{exec_id}/ -mode repairability -repairtool {tool} -output ./{exec_id}_all_out -parameters include_all_instances_for_each_tool:true:executed_by_travis:true:exclude_repair_patterns_not_covering_the_whole_diff:false:maxrevision:500:save_result_revision_analysis:false:output_for_repair:true:max_commit_analysis_time:60000:file_complete_name:true:forbidden_filename_substrings:test:branch:{branch}:CLASSES_HIERARCHY_FILE_NAME:/home/khaes/phd/projects/LighteR/tmp/gumtree-inheritance-relations.txt 1> ./{exec_id}_all_out/stdout.txt 2> ./{exec_id}_all_out/stderr.txt")
+    os.system(
+        f"java -Xmx1g -classpath ./coming.jar fr.inria.coming.main.ComingMain -location ./{exec_id}/ -mode repairability -repairtool {tool} -output ./{exec_id}_all_out -parameters include_all_instances_for_each_tool:true:executed_by_travis:true:exclude_repair_patterns_not_covering_the_whole_diff:false:maxrevision:500:save_result_revision_analysis:false:output_for_repair:true:max_commit_analysis_time:60000:file_complete_name:true:forbidden_filename_substrings:test:branch:{branch}:CLASSES_HIERARCHY_FILE_NAME:/home/khaes/phd/projects/LighteR/tmp/gumtree-inheritance-relations.txt 1> ./{exec_id}_all_out/stdout.txt 2> ./{exec_id}_all_out/stderr.txt")
     os.system(f"rm {exec_id} -rf")
 
     os.system(f"git clone {repoUrl} {exec_id}")
-    os.system(f"java -Xmx1g -classpath ./coming.jar fr.inria.coming.main.ComingMain -location ./{exec_id}/ -mode repairability -repairtool {tool} -output ./{exec_id}_filtered_out -parameters include_all_instances_for_each_tool:true:executed_by_travis:true:exclude_repair_patterns_not_covering_the_whole_diff:true:maxrevision:500:save_result_revision_analysis:false:output_for_repair:true:max_commit_analysis_time:60000:file_complete_name:true:forbidden_filename_substrings:test:branch:{branch}:CLASSES_HIERARCHY_FILE_NAME:/home/khaes/phd/projects/LighteR/tmp/gumtree-inheritance-relations.txt 1> ./{exec_id}_filtered_out/stdout.txt 2> ./{exec_id}_filtered_out/stderr.txt")
+    os.system(
+        f"java -Xmx1g -classpath ./coming.jar fr.inria.coming.main.ComingMain -location ./{exec_id}/ -mode repairability -repairtool {tool} -output ./{exec_id}_filtered_out -parameters include_all_instances_for_each_tool:true:executed_by_travis:true:exclude_repair_patterns_not_covering_the_whole_diff:true:maxrevision:500:save_result_revision_analysis:false:output_for_repair:true:max_commit_analysis_time:60000:file_complete_name:true:forbidden_filename_substrings:test:branch:{branch}:CLASSES_HIERARCHY_FILE_NAME:/home/khaes/phd/projects/LighteR/tmp/gumtree-inheritance-relations.txt 1> ./{exec_id}_filtered_out/stdout.txt 2> ./{exec_id}_filtered_out/stderr.txt")
     os.system(f"rm {exec_id} -rf")
 
     os.system(f"zip ./out/{exec_id}_all_out.zip ./{exec_id}_all_out/*")
@@ -26,6 +30,7 @@ def process(repoName, repoUrl, branch, tool):
     os.system(f"rm {exec_id}_all_out/ -rf")
     os.system(f"rm {exec_id}_filtered_out/ -rf")
 
+
 def main(argv):
     pool = mp.Pool()
 
@@ -33,7 +38,7 @@ def main(argv):
         lines = file.readlines()
         for i in range(len(lines)):
             repo = lines[i].replace("\n", "")
-            repoUrl = f"https://github.com/{repo}.git"
+            repoUrl = f"git@github.com:{repo}.git"
             repoName = repo.replace("/", "__")
             os.system(f"git clone {repoUrl} {repoName}")
             os.chdir(f"{repoName}")
@@ -44,10 +49,11 @@ def main(argv):
             os.chdir("..")
             os.system(f"rm {repoName} -rf")
             for tool in tools:
-                pool.apply_async(process, args = (repoName, repoUrl, branch, tool, ))
+                pool.apply_async(process, args=(repoName, repoUrl, branch, tool,))
 
     pool.close()
     pool.join()
+
 
 if __name__ == "__main__":
     main(sys.argv[1:])
